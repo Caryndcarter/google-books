5b18e02e490dbabd1b115afa2838954a1db7c91e
Caryn Carter <carter.caryn@gmail.com>
2024-12-17 18:14:19 -0600

    modified package.json and routes and server paths

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,9 +11,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
+  // if we're in production, serve client/dist as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+  app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  });
+  
 }
 
 app.use(routes);
